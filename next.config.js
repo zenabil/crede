daf2d59e8a8afb5338f45f7a499a600f87111a68
Zenex <zenexmrkt@gmail.com>
2026-02-02 18:46:57 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,31 @@
+
+const withPWAInit = require('@ducanh2912/next-pwa');
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  scope: '/',
+  sw: 'sw.js',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
