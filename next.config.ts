23a3bd986c9db6271319886f0a6076f0c147fbd7
Zenex <zenexmrkt@gmail.com>
2026-02-02 07:59:26 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://picsum.

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
 export default withPWA(nextConfig);
